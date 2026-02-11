ab2398bc777c52814d8a53adbeb08f4e1d8d423d
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2026-02-11 18:39:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import subprocess
 import os
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from static_ffmpeg import add_paths
-
-add_paths()  # Ensure ffmpeg is in the PATH
 
 app = Flask(__name__)
 CORS(app) # Enable CORS for all routes
